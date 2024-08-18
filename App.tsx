fdfb4f0d0f0c12c3e11b829af5e9dd70977df8e5
Vitor Bizutti <165229421+vitorbizutti@users.noreply.github.com>
2024-08-17 22:13:14 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Home from './src/Screens/Home/Home';
+
+
+
+
+
+
+
+export default function App() {
+  return (
+    
+     <Home/>
+     
+  );
+}
+
+
